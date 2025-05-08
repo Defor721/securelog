4a37cd572225bf4aa0c8408aac4247efa2a6b2be
Defor721 <92020095+Defor721@users.noreply.github.com>
2025-05-08 18:25:56 +0900

    [Feat] 기본설정 수정 및 middleware 작성

--- apps/api/dist/app.module.js
+++ apps/api/dist/app.module.js
@@ -9,15 +9,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
 const app_controller_1 = require("./app.controller");
-const app_service_1 = require("./app.service");
+const prisma_module_1 = require("./prisma/prisma.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [prisma_module_1.PrismaModule],
         controllers: [app_controller_1.AppController],
-        providers: [app_service_1.AppService],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
--- apps/api/dist/main.js
+++ apps/api/dist/main.js
@@ -2,9 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const prisma_service_1 = require("./prisma/prisma.service");
+const logging_middleware_1 = require("./logs/logging.middleware");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(process.env.PORT ?? 3001);
+    const prisma = app.get(prisma_service_1.PrismaService);
+    const logger = new logging_middleware_1.LoggingMiddleware(prisma);
+    app.use(logger.use.bind(logger));
+    await app.listen(3001);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
